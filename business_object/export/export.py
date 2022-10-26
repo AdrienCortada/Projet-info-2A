1b08f5980a514731a44601e1a401d164eac32992
ganisaac <isaacganiyu01@gmail.com>
2022-10-26 11:40:47 +0200

    conflit résolu

--- business_object/export/export.py
+++ business_object/export/export.py
@@ -1,9 +1,8 @@
 from abc import ABC, abstractmethod
 class Export(ABC):
-    def __init__(self,chemin : str, json_file):
+    def __init__(self,chemin : str):
         self.chemin = chemin
-        self.json_file = json_file
-        
+
     @abstractmethod
     def export(self):
         pass

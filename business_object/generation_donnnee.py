3785279f31c922d46de65f05648b36f81af85499
Frazul <abdoul-aziz.toure@eleve.ensai.fr>
2022-10-26 11:01:13 +0200

    genere_donnee

--- business_object/generation_donnnee.py
+++ business_object/generation_donnnee.py
@@ -4,33 +4,31 @@ from business_object.regle_generation.modality import Modality
 from business_object.regle_generation.typ import Type
 import random
 class Generation_donnee:
-    def __init__(self,Nb : int, meta_type : Meta_type, Mod : Modality, types : Type):
+    def __init__(self,Nb : int, meta_type : Meta_type):
         self.Nb = Nb
-        self.Mod = Mod
         self.meta_type = meta_type
-        self.types = types
+       
         
-    def generer_jeu_donnee(self, jeu_donnee):
-        if len(self.jeu_donnee) == 0 :
-            for n in range(Nb):
-                indivivu_n = {}
-                for k in self.types:
-                    tx_r = int(self.types[k]["remplissage"])
-                    if 100*random.random() < tx_r :
-                        mod_list = list(self.Mod[k].values())
-                        mod_list.remove(tx_r)
-                        m = len(mod_list)
-                        nb = random.randint(0, m-1)
-                        mod = mod_list[nb]
-                        indivivu_n[k] = mod
-                    else :
-                        indivivu_n[k] = str(mq)
-                    donnee = {n : indivivu_n}
-                    jeu_donnee.update(donnee)
-            jeu_donnee = json.dumps(jeu_donnee)
-            return jeu_donnee
-        else :
-            return json.dumps(jeu_donnee)            
+    def generer_jeu_donnee(self):
+        donnee = {}
+        for n in range(Nb):
+            indivivu_n = {}
+            for k in self.meta_type:
+                type_individu_n = Type()
+                tx_r = int(type_individu_n[k]["remplissage"])
+                if 100*random.random() < tx_r :
+                    mod_list = list(self.Mod[k].values())
+                    mod_list.remove(tx_r)
+                    m = len(mod_list)
+                    nb = random.randint(0, m-1)
+                    mod = mod_list[nb]
+                    indivivu_n[k] = mod
+                else :
+                    indivivu_n[k] = str(mq)
+                donnee = {n : indivivu_n}
+                jeu_donnee.update(donnee)
+        jeu_donnee = json.dumps(jeu_donnee)
+        return json.dumps(jeu_donnee)            
 
 if __name__ == "__main__":
     import doctest
--- business_object/regle_generation/api.py
+++ business_object/regle_generation/api.py
@@ -29,4 +29,4 @@ async def delete_modality(nom_type : str, value):
     
 
 if __name__ == "__main__":
-    uvicorn.run(app, host = "127.0.0.1", port = 8000)
+    uvicorn.run(app, host = "127.0.0.2", port = 8000)

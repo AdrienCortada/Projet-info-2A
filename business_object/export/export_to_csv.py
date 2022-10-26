d59a51677d2da07544d730219121b3c5e3fe4eca
nathan93600 <nathan936000@gmail.com>
2022-10-26 11:20:46 +0200

    bla

--- business_object/export/export_to_csv.py
+++ business_object/export/export_to_csv.py
@@ -1,1 +1,28 @@
-from export import export
+from export import Export
+import json 
+import csv 
+import pandas as pd 
+
+
+class export_to_csv(Export):
+    def __init__(self,chemin,name):
+        self.chemin = chemin
+        self.name = name
+    def export(self, json_obj):
+        data_json = pd.DataFrame.transpose(json_obj)
+        chemin_f = "{}/{}".format(self.chemin,self.name)
+        file = data_json.to_csv(chemin_f)
+        
+
+        
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    exportation = export_to_csv(r"C:\Users\natha\OneDrive\Documents\ENSAI 2A\UE3 Informatique pour la data science\Projet Informatique","test.csv")
+    json_test = pd.read_json(r"C:\Users\natha\OneDrive\Documents\ENSAI 2A\UE3 Informatique pour la data science\Projet Informatique\Projet-info-2A\json_test.json")
+    exportation.export(json_test)

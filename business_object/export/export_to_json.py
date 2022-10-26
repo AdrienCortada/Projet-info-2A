c82be1a13112fa287ec61b21cc0d45539f9990d0
nathan93600 <nathan936000@gmail.com>
2022-10-26 10:40:33 +0200

    blabla

--- business_object/export/export_to_json.py
+++ business_object/export/export_to_json.py
@@ -1,16 +1,15 @@
+import json
 from export import Export 
 from generation_donnnee import Generation_donnee
 import json
 
-class Export_to_json(self):
-    def __init__(self) -> None:
-        super().__init__()
-    def export(self, chemin, name):
-        f = open(self.generer_jeu_donnee())
-        data = json.load(f)
-        with open("{}{}.json".format(chemin,name), "w") as out_file:
-        json.dump(f, out_file)
-        out_file.close()
+class Export_to_json(Export):
+    def __init__(self,chemin) -> None:
+        super().__init__(chemin)
+    def export(self, json_file, chemin, name):
+        with open("{}/{}.json".format(chemin,name), "w") as out_file:
+            json.dump(json_file, out_file)
+        
 
         
 

1c79efb8b7a11780b5fe09e0a66d64962c1882f3
AdriengCortada <adrien.cortada@gmail.com>
2022-10-26 09:57:42 +0200

    update regle de generation 2

--- business_object/regle_generation/api.py
+++ business_object/regle_generation/api.py
@@ -10,9 +10,15 @@ app = FastAPI()
 @app.get("/add_type/")
 async def add_type(nom : str, tx_remplissage : float):
     t = Type(tx_remplissage, nom)
-    t.add_type()
-    return Type.dict_type
+    return t.add_type()
 
+    
+
+
+@app.delete("/delete_type/")
+async def delete_type(nom : str):
+    Type.delete_type(nom)
+    return Type.dict_type
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "127.0.0.1", port = 8000)

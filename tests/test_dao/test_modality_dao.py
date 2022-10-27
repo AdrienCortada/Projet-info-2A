2af4fbf5cc7c8b37eb8d2a1838568f82e5ab633b
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-10-27 10:42:44 +0200

    update init_db.sql

--- tests/test_dao/test_modality_dao.py
+++ tests/test_dao/test_modality_dao.py
@@ -2,12 +2,12 @@ from dao.db_connection import DBConnection
 from unittest.case import TestCase
 from dao.modality_dao import ModalityDao
 
-#class TestModalityDao(TestCase):
-#
-#    def test_find_all_modality(self):
-#        #GIVEN
-#        modality_dao = ModalityDao()
-#        #WHEN
-#        mods = modality_dao.find_all_modality()
-#        #THEN
-#        self.assertEqual(first, second)
+class TestModalityDao(TestCase):
+
+    def test_find_all_modality(self):
+        #GIVEN
+        modality_dao = ModalityDao()
+        #WHEN
+        mods = modality_dao.find_all_modality()
+        #THEN
+        self.assertEqual(first, second)

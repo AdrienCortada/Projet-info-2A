9b860f095f82008e024f0e569fc8cfda82365723
AdriengCortada <adrien.cortada@gmail.com>
2022-10-27 14:19:22 +0200

    Documentation Modality

--- business_object/regle_generation/modality.py
+++ business_object/regle_generation/modality.py
@@ -1,6 +1,17 @@
 from business_object.regle_generation.typ import Type
 
 class Modality:
+    '''Classe permettant de définir les différents modalités associées à chaque type en les stockant dans un dictionnaire
+    
+    
+    Attributes
+    ----------
+    nom_type : str
+        le nom du type à laquelle est associée la modalité
+    proba d'apparition : float
+        un nombre qui caractérise la probabilité d'appararition d'une modalité parmis les autres modalités qui concernent le même type
+    value
+        la valeur de la modalité'''
 
     dict_modality = {}
 
@@ -11,6 +22,19 @@ class Modality:
         self.nom_type = nom_type
 
     def add_modality(self):
+        '''Fonction qui a pour but d'ajouter une nouvelle modalité au dictionnaire des modalités
+        
+        Example
+        -----------
+        >>> m = Modality("age", 100, 22)
+        >>> print(m.add_modality())
+        The modality has no type associated please check your spelling
+        >>> t = Type(100, "age")
+        >>> t.add_type()
+        {'age': {'remplissage': 100, 'id': 1}}
+        >>> print(m.add_modality())
+        {1: {'type': 'age', 'value': 22, "proba d'apparition": 100}}
+        '''
         if self.nom_type in Type.dict_type :
             id_modality = len(Modality.dict_modality) + 1
             d = {id_modality : {"type" : self.nom_type , "value" : self.value, "proba d'apparition" : self.proba_apparition}}
@@ -21,6 +45,20 @@ class Modality:
         
 
     def delete_modality(nom_type : str, value):
+        '''Fonction qui a pour but d'enlever une modalité au dictionnaire des modalités
+
+        Parameters
+        -----------
+        nom_type : str
+            le nom du type au quel est associé la modalité que l'on veut retirer
+        value 
+            la valeur de la modalité que l'on veut retirer
+        
+        Example
+        -----------
+        >>> print(Modality.delete_modality("age", 22))
+        {1: {'type': 'age', 'value': 22, "proba d'apparition": 0}}
+        '''
         for k in Modality.dict_modality :
             if Modality.dict_modality[k]["value"] == value and Modality.dict_modality[k]["type"] == nom_type:
                 Modality.dict_modality[k]["proba d'apparition"] = 0
@@ -28,3 +66,7 @@ class Modality:
             else : 
                 return "please check your spelling"
 
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    

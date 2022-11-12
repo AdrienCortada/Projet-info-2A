cc2d75719ab8794815d981451578cbaa21070bb6
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-11-12 09:56:07 +0100

    edit test_db_connection

--- tests/test_dao/test_db_connection.py
+++ tests/test_dao/test_db_connection.py
@@ -1,7 +1,7 @@
 from dao.db_connection import DBConnection
-from unittest.case import TestCase
+import unittest
 
-class TestConnection(TestCase):
+class TestConnection(unittest.TestCase):
 
     def test_connection(self):
         with DBConnection().connection as conn:
@@ -9,3 +9,6 @@ class TestConnection(TestCase):
                 cursor.execute("SELECT 1 as test")
                 res = cursor.fetchone()
         self.assertEqual(1,res["test"])
+
+if __name__ == "__main__":
+    unittest.main()

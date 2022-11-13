f8e8c89accda0eb4542f65d9d9a4394e36b78a2a
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-11-13 17:40:55 +0100

    edit meta_dao

--- dao/meta_dao.py
+++ dao/meta_dao.py
@@ -52,81 +52,95 @@ class MetaDao :
                     ids.append(row['id_meta_type'])
         return ids
 
-    def find_meta_by_id(self, id_meta : int):
-        meta_t=[]
+    def add_type_to_meta(self, 
+                         nom_meta_type : str,
+                         nom_type : str):
         with DBConnection().connection as connection:
-            with connection.cursor() as cursor :
+            with connection.cursor() as cursor:
                 cursor.execute(
-                    "SELECT * FROM meta_type WHERE id_meta=%(id_meta)s "
-                    , {"id_meta" : id_meta})
-                res = cursor.fetchall()
-                for row in res:
-                    mt = MetaFactory.get_meta_from_sql_query(row)
-                    meta_t.append(mt)
-        return meta_t
+                    "INSERT INTO meta_type(nom_meta_type, nom_type) VALUES "+
+                    "(%(nom_meta_type)s, %(nom_type)s)",
+                    {"nom_meta_type" : nom_meta_type,
+                     "nom_type" : nom_type}
+                )        
 
-    def find_meta_by_name(self, nom_meta : str):
-        meta_t=[]
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor :
-                cursor.execute(
-                    "SELECT * FROM meta_type "+
-                    "WHERE nom_meta_type=%(nom_meta)s "
-                    , {"nom_meta" : nom_meta})
-                res = cursor.fetchall()
-                for row in res:
-                    mt = MetaFactory.get_meta_from_sql_query(row)
-                    meta_t.append(mt)
-        return meta_t
+    #QUE VEUT-ON POUVOIR FAIRE EXACTEMENT ???
+        
+    #def find_meta_by_id(self, id_meta : int):
+    #    meta_t=[]
+    #    with DBConnection().connection as connection:
+    #        with connection.cursor() as cursor :
+    #            cursor.execute(
+    #                "SELECT * FROM meta_type WHERE id_meta=%(id_meta)s "
+    #                , {"id_meta" : id_meta})
+    #            res = cursor.fetchall()
+    #            for row in res:
+    #                mt = MetaFactory.get_meta_from_sql_query(row)
+    #                meta_t.append(mt)
+    #    return meta_t
 
-    def update_meta_by_id(self, id : int, new_meta : list):
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor :
-                cursor.execute(
-                    "UPDATE meta_type "+
-                    "SET nom_meta_type = %(nom_meta)s, nom_type = %(nom_type)s " +
-                    "WHERE id_meta_type = %(id_meta)s",
-                    {"nom_meta" :new_meta[0],
-                    "nom_type": new_meta[1]}
-                )
-    
-    def update_meta(self, Meta0 : Meta_type, new_meta : Meta_type):  ### Il remplace les n premiers types du meta type avec n la longueur de list_type de new_meta
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor :
-                for i,tip in enumerate(new_meta.list_type):
-                    cursor.execute(
-                        "UPDATE meta_type "+
-                        "SET nom_meta_type = %(nom_meta)s, nom_type = %(nom_type)s " +
-                        "WHERE nom_meta_type = %(nom_m)s AND nom_type = %(type)s ", 
-                        {"nom_meta" :new_meta.nom,
-                        "nom_type": tip,     
-                        "nom_m" : Meta0.nom,
-                        "type" : Meta0.list_type[i]}
-                    )
-    
-    def delete_meta(self, ligne : list):
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor :
-                cursor.execute(
-                    "DELETE FROM meta_type "+
-                    "WHERE id_meta_type IN ( "+
-                    "SELECT id_meta_type FROM meta_type "+
-                    "WHERE nom_meta_type = %(nom_meta_type)s AND nom_type = %(nom_type)s" +
-                    "LIMIT 1 )",
-                    {"nom_meta_type" : ligne[0],
-                    "nom_type": ligne[1]}
-                )
-
-    def delete_meta_by_id(self, id : int):
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor :
-                cursor.execute(
-                    "DELETE FROM meta_type "+
-                    "WHERE id_meta_type = %(id)s",
-                    {"id" : id}
-                )
-    
-
+    #def find_meta_by_name(self, nom_meta : str):
+    #    meta_t=[]
+    #    with DBConnection().connection as connection:
+    #        with connection.cursor() as cursor :
+    #            cursor.execute(
+    #                "SELECT * FROM meta_type "+
+    #                "WHERE nom_meta_type=%(nom_meta)s "
+    #                , {"nom_meta" : nom_meta})
+    #            res = cursor.fetchall()
+    #            for row in res:
+    #                mt = MetaFactory.get_meta_from_sql_query(row)
+    #                meta_t.append(mt)
+    #    return meta_t
+#
+    #def update_meta_by_id(self, id : int, new_meta : list):
+    #    with DBConnection().connection as connection:
+    #        with connection.cursor() as cursor :
+    #            cursor.execute(
+    #                "UPDATE meta_type "+
+    #                "SET nom_meta_type = %(nom_meta)s, nom_type = %(nom_type)s " +
+    #                "WHERE id_meta_type = %(id_meta)s",
+    #                {"nom_meta" :new_meta[0],
+    #                "nom_type": new_meta[1]}
+    #            )
+    #
+    #def update_meta(self, Meta0 : Meta_type, new_meta : Meta_type):  ### Il remplace les n premiers types du meta type avec n la longueur de list_type de new_meta
+    #    with DBConnection().connection as connection:
+    #        with connection.cursor() as cursor :
+    #            for i,tip in enumerate(new_meta.list_type):
+    #                cursor.execute(
+    #                    "UPDATE meta_type "+
+    #                    "SET nom_meta_type = %(nom_meta)s, nom_type = %(nom_type)s " +
+    #                    "WHERE nom_meta_type = %(nom_m)s AND nom_type = %(type)s ", 
+    #                    {"nom_meta" :new_meta.nom,
+    #                    "nom_type": tip,     
+    #                    "nom_m" : Meta0.nom,
+    #                    "type" : Meta0.list_type[i]}
+    #                )
+    #
+    #def delete_meta(self, ligne : list):
+    #    with DBConnection().connection as connection:
+    #        with connection.cursor() as cursor :
+    #            cursor.execute(
+    #                "DELETE FROM meta_type "+
+    #                "WHERE id_meta_type IN ( "+
+    #                "SELECT id_meta_type FROM meta_type "+
+    #                "WHERE nom_meta_type = %(nom_meta_type)s AND nom_type = %(nom_type)s" +
+    #                "LIMIT 1 )",
+    #                {"nom_meta_type" : ligne[0],
+    #                "nom_type": ligne[1]}
+    #            )
+#
+    #def delete_meta_by_id(self, id : int):
+    #    with DBConnection().connection as connection:
+    #        with connection.cursor() as cursor :
+    #            cursor.execute(
+    #                "DELETE FROM meta_type "+
+    #                "WHERE id_meta_type = %(id)s",
+    #                {"id" : id}
+    #            )
+    #
+#
 
 if __name__ == '__main__':
     print('Tests de la classe MetaDao')
@@ -143,5 +157,7 @@ if __name__ == '__main__':
     #ids = MetaDao().find_ids_meta(nom_meta = 'meta_test')
     #print(ids)
 
-    #Test de find_meta_by_name
+    #Test de add_type_to_meta
+    MetaDao().add_type_to_meta(nom_meta_type = 'meta_test',
+                               nom_type = 'nom commune')
     

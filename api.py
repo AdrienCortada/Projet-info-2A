345a109dd0c8e8db7abc46fc7f213d399e9919b3
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-11-18 16:18:44 +0100

    edit api et data_factory

--- api.py
+++ api.py
@@ -27,6 +27,7 @@ from dao.typ_dao import TypeDao
 
 
 
+
 app = FastAPI()
 
 @app.get("/get_dict_type")
@@ -117,7 +118,7 @@ async def save_data_dao():
     meta = MetaDao()
     typ = TypeDao()
     modalite = ModalityDao()
-    meta.save_meta(Meta_type(Meta_type.meta_type1[-1], Meta_type.dict_meta_type[Meta_type.meta_type1[-1]]))
+    meta.save_meta(Meta_type(Genaration_donnee.meta_type1[-1], Meta_type.dict_meta_type[Generation_donnee.meta_type1[-1]]))
     for elt in Meta_type.dict_meta_type[Meta_type.meta_type1[-1]] : 
         typ.save_type(Type(Type.dict_type[elt]["remplissage"], elt))
     for mod in Modality.dict_modality :

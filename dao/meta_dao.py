50b174eb075ff65966652dd247b1c9de7ebd6c83
Laurène Villacampa <laurene.villacampa@eleve.ensai.fr>
2022-11-19 17:40:22 +0100

    edit meta_dao

--- dao/meta_dao.py
+++ dao/meta_dao.py
@@ -51,31 +51,31 @@ class MetaDao :
                 for row in res:
                     ids.append(row['id_meta_type'])
         return ids
-
-    def add_type_to_meta(self, 
-                         nom_meta_type : str,
-                         nom_type : str):
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO meta_type(nom_meta_type, nom_type) VALUES "+
-                    "(%(nom_meta_type)s, %(nom_type)s)",
-                    {"nom_meta_type" : nom_meta_type,
-                     "nom_type" : nom_type}
-                )    
+ 
 
     def delete_meta_by_name(self, nom_meta_type : str):
         with DBConnection().connection as connection:
             with connection.cursor() as cursor :
                 cursor.execute(
                     "DELETE FROM meta_type "+
-                    "WHERE nom_meta_type = %(nom_meta_type)s",
-                    {"nom_meta_type" : nom_meta_type}
+                    "WHERE nom_meta_type = %(nom_meta)s",
+                    {"nom_meta" : nom_meta_type}
                 )    
 
+    def find_meta_by_name(self, nom_meta : str):
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor :
+                cursor.execute(
+                    "SELECT * FROM meta_type "+
+                    "WHERE nom_meta_type=%(nom_meta)s "
+                    , {"nom_meta" : nom_meta})
+                res = cursor.fetchall()
+                mt = MetaFactory.get_meta_type_from_sql_query(res, nom_meta)
+        return mt
+
     #QUE VEUT-ON POUVOIR FAIRE EXACTEMENT ???
-        
-    #def find_meta_by_id(self, id_meta : int):
+  
+    #def find_meta_by_id(self, id_meta : int):      #Cette méthode ne renvoie qu'un type associé à un méta ?  
     #    meta_t=[]
     #    with DBConnection().connection as connection:
     #        with connection.cursor() as cursor :
@@ -88,19 +88,7 @@ class MetaDao :
     #                meta_t.append(mt)
     #    return meta_t
 
-    #def find_meta_by_name(self, nom_meta : str):
-    #    meta_t=[]
-    #    with DBConnection().connection as connection:
-    #        with connection.cursor() as cursor :
-    #            cursor.execute(
-    #                "SELECT * FROM meta_type "+
-    #                "WHERE nom_meta_type=%(nom_meta)s "
-    #                , {"nom_meta" : nom_meta})
-    #            res = cursor.fetchall()
-    #            for row in res:
-    #                mt = MetaFactory.get_meta_from_sql_query(row)
-    #                meta_t.append(mt)
-    #    return meta_t
+
 #
     #def update_meta_by_id(self, id : int, new_meta : list):
     #    with DBConnection().connection as connection:
@@ -155,6 +143,8 @@ class MetaDao :
 
 if __name__ == '__main__':
     print('Tests de la classe MetaDao')
+
+    #Test de find_all_meta
     #metas = MetaDao().find_all_meta()
     #print(len(metas) == 2)
 
@@ -168,10 +158,9 @@ if __name__ == '__main__':
     #ids = MetaDao().find_ids_meta(nom_meta = 'meta_test')
     #print(ids)
 
-    #Test de add_type_to_meta
-    #MetaDao().add_type_to_meta(nom_meta_type = 'meta_test',
-    #                           nom_type = 'nom commune')
-
     #Test de delete_meta_by_name
-    MetaDao().delete_meta_by_name('meta_test')
-    
+    #MetaDao().delete_meta_by_name('meta_test')
+
+    #test de find_meta_by_name
+    #meta = MetaDao().find_meta_by_name('commune')
+    #print(meta.nom, meta.list_type)

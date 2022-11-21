25b5e681efd99315cbf1f5b13fcf7405a331661c
ganisaac <isaacganiyu01@gmail.com>
2022-11-22 00:17:05 +0100

    tentative de débodgage delete_row

--- dao/data_dao.py
+++ dao/data_dao.py
@@ -108,10 +108,11 @@ class DataDao :
         with DBConnection().connection as connection:
             with connection.cursor() as cursor :
                 cursor.execute(
-                    "DELETE "+
+                    "DELETE FROM ("+
+                    "SELECT id_donnee, nom_meta_type, nom_type, order_donnee, value_donnee "+
                     "FROM ( (SELECT MIN(id_donnee) as min FROM donnee WHERE nom_meta_type = %(nom_meta)s ) as tamp1 "+
                     "CROSS JOIN (SELECT * FROM donnee WHERE nom_meta_type = %(nom_meta)s) AS tamp2 ) AS tamp "+
-                    "WHERE id_donnee >= min+%(n_col)s*(%(i_row)s-1) AND id_donnee<min+%(n_col)s*%(i_row)s"
+                    "WHERE id_donnee >= min+%(n_col)s*(%(i_row)s-1) AND id_donnee<min+%(n_col)s*%(i_row)s)"
                     , {"i_row" : i_row,
                         "n_col" : nb_col,
                         "nom_meta" : nom_meta})

9a42308a1714e0e43c7a8baa8c95e707a9bcb654
AdriengCortada <adrien.cortada@gmail.com>
2022-11-24 12:26:50 +0100

    changement sur les tests

--- tests/test_business_object/test_regle_generation/test_typ.py
+++ tests/test_business_object/test_regle_generation/test_typ.py
@@ -1,17 +1,19 @@
+import unittest
 from business_object.regle_generation.typ import Type
 
+class TestType(unittest.TestCase):
+
+    def test_add_type(self):
+        t = Type(100, "age")
+        dic1 = t.add_type()
+        self.assertEqual(dic1, {'age': {'remplissage': 100, 'id': 1}})
+
+    def test_delete_type(self):
+        dic2 = Type.delete_type("age")
+        self.assertEqual(dic2, {})
 
 
-if __name__ == "__main__":
-    t = Type(100, "age")
-    dic1 = t.add_type()
-    if dic1 == {'age': {'remplissage': 100, 'id': 1}}:
-        print("True") 
-    else:
-        print("False")
-    dic2 = Type.delete_type("age")
-    if dic2 == {}:
-        print("True") 
-    else:
-        print("False")
 
+
+if __name__ == '__main__':
+    unittest.main()
